d2b92c685669d1edfaf09eb2b7b05dcd6d905281
Willy Scheibel <willy.scheibel@hpi.de>
2022-02-16 19:57:18 +0100

    Add rate limits for infinite generation of data

--- service.py
+++ service.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app, header_key="X-DATA-GENERATOR-KEY", header_value="e5f7b5ea3fea53df2266a64ade09b75a7f3273d0fb1dce11edd90a422728a06e"):
+        super().__init__(app)
+        self.header_key = header_key
+        self.header_value = header_value
+    
+    async def dispatch(self, request, call_next):
+        rate_unlimited = (self.header_key in request.headers) and (request.headers[self.header_key] == self.header_value)
+        request.scope['rate_unlimited'] = rate_unlimited
+        return await call_next(request)
 
 from v0 import router as v0_router
 
@@ -8,6 +20,8 @@ app.include_router(
     v0_router,
     # prefix="/v0",
 )
+
+app.add_middleware(RateLimitMiddleware)
 
 
 if __name__ == "__main__":
--- v0/__init__.py
+++ v0/__init__.py
@@ -1,10 +1,14 @@
 
-from fastapi import APIRouter, Response, BackgroundTasks, File, UploadFile
+import os
+import distutils
+
+from fastapi import APIRouter, Request, Response, BackgroundTasks, File, UploadFile
 from fastapi import HTTPException, status, Depends, WebSocket
 from fastapi.responses import FileResponse, StreamingResponse, PlainTextResponse
 
 import random
 import websockets
+
 
 router = APIRouter()
 
@@ -39,6 +43,16 @@ def generate_full_infinite(break_check):
     yield from generate_infinite(break_check)
 
 
+try:
+    global_rate_unlimited = distutils.util.strtobool(os.environ.get('DATA_GENERATOR_SERVICE_RATE_UNLIMITED', ''))
+except:
+    global_rate_unlimited = False
+
+def is_rate_unlimited(request: Request):
+    return global_rate_unlimited or \
+        ("rate_unlimited" in request.scope) and request.scope["rate_unlimited"]
+
+
 # ROOT
 
 @router.get("/", tags=[""])
@@ -52,13 +66,13 @@ async def api_post_seed(seed: str):
 
 
 @router.get("/csv/file/as/download", tags=["csv", "file", "download"])
-async def api_get_csv_download():
+async def api_get_csv_file_download():
 
     return FileResponse("./data/test.csv", filename="test.csv", media_type="text/csv")
 
 
 @router.get("/csv/file/as/stream", tags=["csv", "file", "stream"])
-async def api_get_csv_download():
+async def api_get_csv_file_stream():
 
     def file_streaming():
         with open("./data/test.csv", mode="r") as file_like:
@@ -67,8 +81,25 @@ async def api_get_csv_download():
     return StreamingResponse(file_streaming(), media_type="text/csv")
 
 
-@router.get("/csv/generator_n/as/download", tags=["csv", "generator", "stream"])
-async def api_get_csv_download(n: int = 100):
+@router.websocket("/csv/file/as/websocket")
+async def api_get_csv_file_websocket(websocket: WebSocket):
+    await websocket.accept()
+
+    def file_streaming():
+        with open("./data/test.csv", mode="r") as file_like:
+            yield from file_like
+    
+    try:
+        for s in file_streaming():
+            await websocket.send_text(s)
+        
+        await websocket.close()
+    except websockets.exceptions.ConnectionClosedError:
+        pass
+
+
+@router.get("/csv/generator_n/as/download", tags=["csv", "generator", "download"])
+async def api_get_csv_generator_n_download(n: int = 100):
     content = ""
     for s in generate_full_n(n):
         content += s
@@ -77,13 +108,13 @@ async def api_get_csv_download(n: int = 100):
 
 
 @router.get("/csv/generator_n/as/stream", tags=["csv", "generator", "stream"])
-async def api_get_csv_download(n: int = 100):
+async def api_get_csv_generator_n_stream(n: int = 100):
 
     return StreamingResponse(generate_full_n(n), media_type="text/csv")
 
 
 @router.websocket("/csv/generator_n/as/websocket")
-async def api_get_csv_download(websocket: WebSocket, n: int = 100):
+async def api_get_csv_generator_n_websocket(websocket: WebSocket, n: int = 100):
     await websocket.accept()
     
     try:
@@ -96,20 +127,40 @@ async def api_get_csv_download(websocket: WebSocket, n: int = 100):
 
 
 @router.get("/csv/generator_infinite/as/stream", tags=["csv", "generator", "stream"])
-async def api_get_csv_download():
+async def api_get_csv_generator_infinite_stream(request: Request):
+    rate_unlimited = is_rate_unlimited(request)
+    n = 0
+
     def break_check():
-        return False
+        nonlocal rate_unlimited
+        nonlocal n
+
+        if rate_unlimited:
+            return False
+        
+        n += 1
+        return n > 100
 
     return StreamingResponse(generate_full_infinite(break_check), media_type="text/csv")
 
 
 @router.websocket("/csv/generator_infinite/as/websocket")
-async def api_get_csv_download(websocket: WebSocket):
+async def api_get_csv_generator_infinite_websocket(websocket: WebSocket):
+    rate_unlimited = is_rate_unlimited(websocket)
+    n = 0
+
     def break_check():
-        return False
+        nonlocal rate_unlimited
+        nonlocal n
+
+        if rate_unlimited:
+            return False
+        
+        n += 1
+        return n > 100
     
     await websocket.accept()
-    
+
     try:
         for s in generate_full_infinite(break_check):
             await websocket.send_text(s)
